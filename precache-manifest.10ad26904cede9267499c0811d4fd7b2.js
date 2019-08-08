b0051d0a4ce2c0f00b16234e4e5d7780c5a8a946
awnigharbia <awni2009@hotmail.com>
2019-08-08 19:14:58 +0300

    Updates

---
+++ precache-manifest.10ad26904cede9267499c0811d4fd7b2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a6d26b93f94035278b1",
+    "url": "/fadfada/static/css/main.e51bfd88.chunk.css"
+  },
+  {
+    "revision": "7a6d26b93f94035278b1",
+    "url": "/fadfada/static/js/main.8f6808b0.chunk.js"
+  },
+  {
+    "revision": "3848c671edee50205d8a",
+    "url": "/fadfada/static/js/runtime~main.138eda61.js"
+  },
+  {
+    "revision": "f61cff09d3bd78caa8c2",
+    "url": "/fadfada/static/js/2.0a91d9e4.chunk.js"
+  },
+  {
+    "revision": "da5c33a0d45bb1f3b31a",
+    "url": "/fadfada/static/js/3.34948b05.chunk.js"
+  },
+  {
+    "revision": "8b33b22920514e7ead6b8635a751ac07",
+    "url": "/fadfada/static/media/form.8b33b229.png"
+  },
+  {
+    "revision": "86377fca9a2188d4d1eaafa1576cf3e1",
+    "url": "/fadfada/static/media/doodles.86377fca.png"
+  },
+  {
+    "revision": "d8a9fd249896e64ee91186224cb83f0e",
+    "url": "/fadfada/static/media/logo.d8a9fd24.webp"
+  },
+  {
+    "revision": "0404b6e9bce0aadedde55fb65e8994cf",
+    "url": "/fadfada/static/media/small.0404b6e9.png"
+  },
+  {
+    "revision": "068b1c3456f23d56ea00acdb6893004b",
+    "url": "/fadfada/static/media/world-wide.068b1c34.svg"
+  },
+  {
+    "revision": "44d5cf6605e726aa1f8b307fe6910c0a",
+    "url": "/fadfada/static/media/notification.44d5cf66.jpg"
+  },
+  {
+    "revision": "5d721673d3256e73ab0df02a43a80288",
+    "url": "/fadfada/static/media/notification-msg.5d721673.png"
+  },
+  {
+    "revision": "699a63819404a682efa6ed2a8d58f0b5",
+    "url": "/fadfada/index.html"
+  }
+];
