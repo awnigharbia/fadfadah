1c68a6a9169c843cf49242f94afe5b7f5c59237e
awnigharbia <awni2009@hotmail.com>
2019-08-11 14:17:20 +0300

    fixing modal issues

--- src/wall/widgets/support.js
+++ src/wall/widgets/support.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import '../css/Wall.css'
 import {Support, FormClose, StatusGood} from 'grommet-icons'
 import {useSetState} from '../../hooks/useSetState'
@@ -6,6 +6,8 @@ import {useMutation} from 'react-apollo-hooks'
 import gql from 'graphql-tag'
 import _ from 'lodash'
 import TextareaAutosize from 'react-autosize-textarea'
+import useOnClickOutside from '../../hooks/useOutsideClick'
+import useLockBodyScroll from '../../hooks/useLockBodyScroll'
 
 const SEND_FEEDBACK = gql`
   mutation sendFeedback($topic: String!, $description: String!) {
@@ -17,6 +19,7 @@ const SEND_FEEDBACK = gql`
 
 export default function SupportWidget() {
   const sendFeedback = useMutation(SEND_FEEDBACK)
+
   const [state, setState] = useSetState({
     modal: false,
     topics: ['Personal', 'Technical', 'Content', 'Bug', 'Other'],
@@ -44,71 +47,11 @@ export default function SupportWidget() {
   return (
     <div className="support-wrapper">
       {state.modal && (
-        <div className="modal">
-          <div className="support-modal-content">
-            <div className="topbar">
-              <p>Send Feedback</p>
-              <FormClose
-                size="medium"
-                color="gray"
-                style={{
-                  height: '25px',
-                  width: '25px',
-                  margin: '5px',
-                  borderRadius: '50%',
-                  backgroundColor: 'hsl(210, 0%, 92%)',
-                }}
-                onClick={() => setState({modal: false, done: false})}
-              />
-            </div>
-            <p id="sec-title">Select Topic:</p>
-            <div className="issue-topics">
-              {state.topics.map((i, key) => (
-                <Topic
-                  name={i}
-                  key={key}
-                  active={key === state.activeTopic}
-                  onClick={() => setState({activeTopic: key})}
-                />
-              ))}
-            </div>
-            <div className="issue-field">
-              <TextareaAutosize
-                rows={10}
-                value={state.description}
-                placeholder="Write your issue.."
-                onChange={e => setState({description: e.target.value})}
-                style={{
-                  width: '100%',
-                  border: 'none',
-                  padding: '10px 20px',
-                  boxSizing: 'border-box',
-                  outline: 'none',
-                  resize: 'none',
-                  fontSize: '13.5px',
-                  fontWeight: '500',
-                  letterSpacing: '1px',
-                }}
-                autoFocus
-              />
-            </div>
-            <div className="issue-btn">
-              <button
-                id="feedback-close"
-                onClick={() => setState({modal: false, done: false})}
-              >
-                Close
-              </button>
-              <button
-                id="feedback-send"
-                disabled={state.description.length === 0 && state.done}
-                onClick={handleFeedback}
-              >
-                {state.done ? 'Sent!' : 'Send Feedback'}
-              </button>
-            </div>
-          </div>
-        </div>
+        <Modal
+          setState={setState}
+          state={state}
+          handleFeedback={handleFeedback}
+        />
       )}
       <div className="left">
         <Support size="large" color="hsl(202, 57%, 15%)" />
@@ -122,6 +65,7 @@ export default function SupportWidget() {
   )
 }
 
+////// Topic Component ////////
 function Topic({name, active, onClick}) {
   return (
     <div
@@ -134,6 +78,80 @@ function Topic({name, active, onClick}) {
         style={{marginRight: '5px'}}
       />
       {name}
+    </div>
+  )
+}
+
+///////Modal Component////////
+function Modal({setState, handleFeedback, state}) {
+  const ref = useRef()
+  useOnClickOutside(ref, () => setState({modal: false}))
+  useLockBodyScroll()
+  return (
+    <div className="modal">
+      <div className="support-modal-content" ref={ref}>
+        <div className="topbar">
+          <p>Send Feedback</p>
+          <FormClose
+            size="medium"
+            color="gray"
+            style={{
+              height: '25px',
+              width: '25px',
+              margin: '5px',
+              borderRadius: '50%',
+              backgroundColor: 'hsl(210, 0%, 92%)',
+            }}
+            onClick={() => setState({modal: false, done: false})}
+          />
+        </div>
+        <p id="sec-title">Select Topic:</p>
+        <div className="issue-topics">
+          {state.topics.map((i, key) => (
+            <Topic
+              name={i}
+              key={key}
+              active={key === state.activeTopic}
+              onClick={() => setState({activeTopic: key})}
+            />
+          ))}
+        </div>
+        <div className="issue-field">
+          <TextareaAutosize
+            rows={10}
+            value={state.description}
+            placeholder="Write your issue.."
+            onChange={e => setState({description: e.target.value})}
+            style={{
+              width: '100%',
+              border: 'none',
+              padding: '10px 20px',
+              boxSizing: 'border-box',
+              outline: 'none',
+              resize: 'none',
+              fontSize: '13.5px',
+              fontWeight: '500',
+              letterSpacing: '1px',
+            }}
+            autoFocus
+          />
+        </div>
+        <div className="issue-btn">
+          <button
+            id="feedback-close"
+            onClick={() => setState({modal: false, done: false})}
+          >
+            Close
+          </button>
+          <button
+            id="feedback-send"
+            disabled={state.description.length === 0 && state.done}
+            onClick={handleFeedback}
+          >
+            {state.done ? 'Sent!' : 'Send Feedback'}
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
